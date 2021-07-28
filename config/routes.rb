e7ee9b7a0d87da9ebc6117d3773f5666c11fdafd
Jun-Kataoka <j.kataoka.biz@gmail.com>
2021-07-28 14:06:43 +0900

    カード登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   get 'items/index'
   devise_for :users
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
+  resources :items, only: :order do
+    post 'order', on: :member
+  end
 end
